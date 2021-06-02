24501ef6a6a3219e7cc4d0c5a5f5df25faf8eda1
Patrick Organ <patrick@permafrost.dev>
2021-06-02 16:01:14 -0400

    apply formatting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import { App } from '@tinyhttp/app'
-import graphql from 'graphql'
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { App } from '@tinyhttp/app';
+import graphql from 'graphql';
 //import expressGraphQL from 'express-graphql'
 
-import bodyParser from "body-parser"
-import { ApolloServer } from "apollo-server-express"
-import typeDefs from "./typedef.js"
-import resolvers from "./resolver.js"
+import bodyParser from 'body-parser';
+import { ApolloServer } from 'apollo-server-express';
+import typeDefs from './typedef.js';
+import resolvers from './resolver.js';
 
-const app = new App()
-const port = parseInt(process.env.PORT) || 3030
+const app = new App();
+const port = parseInt(process.env.PORT) || 3030;
 
 /*
 const schema = graphql.buildSchema(`
@@ -31,24 +34,23 @@ app.use(
 )
 */
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const server = new ApolloServer({
-  introspection: true,
-  typeDefs,
-  resolvers,
-  formatError: error => {
-    return error
-  },
-  context: ({ req, res }) => {
-    return {
-      req,
-      res,
-    }
-  },
-})
+    introspection: true,
+    typeDefs,
+    resolvers,
+    formatError: error => {
+        return error;
+    },
+    context: ({ req, res }) => {
+        return {
+            req,
+            res,
+        };
+    },
+});
 
-server.applyMiddleware({ app, path: "/graphql" })
+server.applyMiddleware({ app, path: '/graphql' });
 
-
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`))
+app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
--- src/typedef.js
+++ src/typedef.js
@@ -1,45 +1,45 @@
-import { gql } from "apollo-server-express"
+import { gql } from 'apollo-server-express';
 const typeDefs = gql`
-  type Story {
-    by: String
-    descendants: Int
-    kids: [Int]
-    score: Int
-    time: Int
-    title: String
-    Type: String
-    url: String
-  }
-  type Author {
-    id: Int!
-    firstName: String
-    lastName: String
-    posts: [Post]
-  }
-  type Post {
-    id: Int!
-    title: String
-    authorId: ID!
-    votes: Int
-    author: Author
-  }
-  input PostData {
-    id: Int!
-    title: String
-    authorId: ID!
-    votes: Int
-  }
-  type Response {
-    success: Boolean
-  }
-  type Query {
-    posts: [Post]
-    authors(first: Int!): [Author]
-    author(id: Int!): Author
-    stories(first: Int!): [Story]
-  }
-  type Mutation {
-    createPost(post: PostData): Response
-  }
-`
-export default typeDefs
+    type Story {
+        by: String
+        descendants: Int
+        kids: [Int]
+        score: Int
+        time: Int
+        title: String
+        Type: String
+        url: String
+    }
+    type Author {
+        id: Int!
+        firstName: String
+        lastName: String
+        posts: [Post]
+    }
+    type Post {
+        id: Int!
+        title: String
+        authorId: ID!
+        votes: Int
+        author: Author
+    }
+    input PostData {
+        id: Int!
+        title: String
+        authorId: ID!
+        votes: Int
+    }
+    type Response {
+        success: Boolean
+    }
+    type Query {
+        posts: [Post]
+        authors(first: Int!): [Author]
+        author(id: Int!): Author
+        stories(first: Int!): [Story]
+    }
+    type Mutation {
+        createPost(post: PostData): Response
+    }
+`;
+export default typeDefs;
