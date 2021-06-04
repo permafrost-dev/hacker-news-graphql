ef493113dc5947b2410b2c434e73d028ebe97473
Patrick Organ <patrick@permafrost.dev>
2021-06-04 17:33:23 -0400

    wip

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import resolvers from './resolver';
 require('dotenv').config();
 
 const app = new App();
+
 const port = parseInt(process.env.PORT) || 3000;
 const isDevelopmentMode = process.env.NODE_ENV === 'development';
 
